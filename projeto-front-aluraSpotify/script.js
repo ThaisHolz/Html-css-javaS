60d8a79e36a350275972c8892288adc16961d677
Thais Coelho <thaisholz@hotmail.com>
2024-01-29 16:09:28 +0000

    Tentando corrigir buscar por artistas

--- projeto-front-aluraSpotify/script.js
+++ projeto-front-aluraSpotify/script.js
@@ -1,29 +1,35 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
+const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists`
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result,searchTerm))
 }
 
-function displayResults(result) {
-    resultPlaylist.classList.add('hidden');
+function displayResults(result, TermoDeBusca) {
+    result.filter(function(artist){
+        return artist.name.toLowerCase().includes(TermoDeBusca.toLowerCase());
+    });   
+    
+    resultPlaylist.classList.add("hidden");
     const artistName = document.getElementById('artist-name');
+    const artistCategory = document.getElementById('artist-categorie');
     const artistImage = document.getElementById('artist-img');
 
     result.forEach(element => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
+        artistCategory.innerText = "Artista";
     });
 
     resultArtist.classList.remove('hidden');
 }
 
 document.addEventListener('input', function() {
-    const searchTerm = searchInput.value.toLowerCase();
+    const searchTerm = searchInput.value.toLowerCase;
     if(searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
